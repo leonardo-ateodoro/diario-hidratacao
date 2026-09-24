224745d61e65ff423484d768b06d4845269ae802
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-09-24 09:01:29 -0300

    terminei o colors

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+        background: '#F0F9FF',
+        cardBg: '#FFFFFF', 
+        primary: '#0284c7', 
+        secondary: '#38BDF8', 
+        textMain:  '#0c4a6e', 
+        textMuted: '#64748b',
+        danger: '#ef4444', 
+        white : "#ffffff", 
+};
